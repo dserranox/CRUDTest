bcf16bf1184d8c5921d938777c7f81d383d6a457
dserrano <dserrano@trimix.com.ar>
2022-11-23 12:16:12 -0300

    first commit

---
+++ src/test/java/com/example/CRUDTest/CrudTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CRUDTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
